a899d56fc63278bc16190c000a01309180ffdfa3
Conrad VanLandingham <cv@utexas.edu>
2015-04-07 09:19:06 -0500

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,31 +4,31 @@ var keysUp = [38, 33, 36, 37];
 var keysDown = [40, 34, 32, 39];
 
 // on key up or key down trigger a sectionScroll with maximum inertia
-$(window).on("keydown", function(e) {
-  for (var i = keysUp.length; i--;) {
-    if (e.keyCode === keysUp[i]) {
-      $(window).scrollLeft($(window).scrollLeft()-25);
-      return;
-    }
-  }
-  for (var i = keysDown.length; i--;) {
-    if (e.keyCode === keysDown[i]) {
-      $(window).scrollLeft($(window).scrollLeft()+25);
-      return;
-    }
-  }
-});
+// $(window).on("keydown", function(e) {
+//   for (var i = keysUp.length; i--;) {
+//     if (e.keyCode === keysUp[i]) {
+//       $(window).scrollLeft($(window).scrollLeft()-25);
+//       return;
+//     }
+//   }
+//   for (var i = keysDown.length; i--;) {
+//     if (e.keyCode === keysDown[i]) {
+//       $(window).scrollLeft($(window).scrollLeft()+25);
+//       return;
+//     }
+//   }
+// });
 
 
-// on scrollwheel
-function wheel(e) {
-  var delta = -1 * Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-  $(window).scrollLeft($(window).scrollLeft()+(delta*25));
-}
-if (window.addEventListener) {
-  window.addEventListener('DOMMouseScroll', wheel, false);
-}
-window.onmousewheel = document.onmousewheel = wheel;
+// // on scrollwheel
+// function wheel(e) {
+//   var delta = -1 * Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+//   $(window).scrollLeft($(window).scrollLeft()+(delta*25));
+// }
+// if (window.addEventListener) {
+//   window.addEventListener('DOMMouseScroll', wheel, false);
+// }
+// window.onmousewheel = document.onmousewheel = wheel;
 
 
 // Sidemenu
@@ -36,7 +36,9 @@ $('.sidemenu').slicknav({
     prependTo:'.main-nav'
 });
 
-$(".slicknav_nav a").click(function(e){
-  e.preventDefault();
-  $('html, body').animate({scrollLeft: $($(e.target).attr("href")).offset().left });
+$("a[href^=#]").click(function(e){
+  if($($(e.target).attr("href")).length) {
+    e.preventDefault();
+    $('html, body').animate({scrollLeft: $($(e.target).attr("href")).offset().left });
+  }
 });
